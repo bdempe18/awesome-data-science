c72d373a121fd35ab13ed74a4eb25eb307753fe4
bdempe <dempeb18@gmail.com>
2021-05-09 09:31:22 -0400

    require statsplots

--- julia/requirements.jl
+++ julia/requirements.jl
@@ -5,6 +5,7 @@ dependencies = [
     "Plot",             # Default plotting
     "DataFrames",       # Data tables
     "Revise",           # Edit package bugs without restarting julia"
+    "StatsPlots"        # kernal density, @df macro
    ]
 
 Pkg.add(dependencies)
