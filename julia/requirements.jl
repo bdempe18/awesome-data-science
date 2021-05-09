12aed035ad556d18a59677dabd566fcef7916c4f
bdempe <dempeb18@gmail.com>
2021-05-09 10:47:43 -0400

    Update requirements.jl

--- julia/requirements.jl
+++ julia/requirements.jl
@@ -5,7 +5,8 @@ dependencies = [
     "Plot",             # Default plotting
     "DataFrames",       # Data tables
     "Revise",           # Edit package bugs without restarting julia"
-    "StatsPlots"        # kernal density, @df macro
+    "StatsPlots"        # kernal density, @df,
+    "MultivariateStats" # PCA
    ]
 
 Pkg.add(dependencies)
